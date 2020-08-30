86a507b527f2c13e976174e23f13ccc4bf10c58b
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-08-30 18:42:31 -0300

    tela do contato

---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:agenda_contato/helpers/contact_helper.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  Contact _editedContact;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.contact == null) {
+      _editedContact = Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
