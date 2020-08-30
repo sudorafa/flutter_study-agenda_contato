63944afb244dae67aab6a5773371216bddd21719
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-08-30 17:39:07 -0300

    configuracao paginas e testado banco

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:agenda_contato/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ContactHelper helper = ContactHelper();
+
+  List<Contact> contacts = List();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
