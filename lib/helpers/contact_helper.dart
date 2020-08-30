16f6fbb50efde253f1dea147c57ea2e8478305ce
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-08-30 17:15:21 -0300

    criado molde contato

---
+++ lib/helpers/contact_helper.dart
@@ -0,0 +1,44 @@
+final String contactTable = "contactTable";
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imgColumn = "imgColumn";
+
+class ContactHelper {}
+
+class Contact {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String img;
+
+  Contact();
+
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img
+    };
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
+  }
+}
