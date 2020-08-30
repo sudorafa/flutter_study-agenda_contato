71362d5f13b9faf6bb39d19d06fad4bcb4aedd20
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-08-30 18:57:12 -0300

    chamando tela para add contato, passando contato entre telas

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
-
 import 'package:agenda_contato/helpers/contact_helper.dart';
+import 'package:agenda_contato/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -30,63 +29,34 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _showContactPage();
+        },
         child: Icon(Icons.add),
         backgroundColor: Colors.red,
       ),
       body: ListView.builder(
           padding: EdgeInsets.all(10.0),
           itemCount: contacts.length,
-          itemBuilder: (context, index) {
-            {}
-          }),
+          itemBuilder: (context, index) {}),
     );
   }
 
-  Widget _contactCard(BuildContext context, int index) {
-    return GestureDetector(
-        child: Card(
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Row(
-          children: <Widget>[
-            Container(
-              width: 80.0,
-              height: 80.0,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    image: contacts[index].img != null
-                        ? FileImage(File(contacts[index].img))
-                        : AssetImage("images/person.png"),
-                    fit: BoxFit.cover),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    contacts[index].name ?? "",
-                    style:
-                        TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    contacts[index].email ?? "",
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                  Text(
-                    contacts[index].phone ?? "",
-                    style: TextStyle(fontSize: 18.0),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    ));
+  void _showContactPage({Contact contact}) async {
+    final recContact = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ContactPage(
+                  contact: contact,
+                )));
+    if (recContact != null) {
+      if (contact != null) {
+        await helper.updateContact(recContact);
+      } else {
+        await helper.saveContact(recContact);
+      }
+      _getAllContacts();
+    }
   }
 
   void _getAllContacts() {
